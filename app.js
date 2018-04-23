e7b49c304e8c66bf6ccffe96f03a1463dce72a0b
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-23 13:21:26 +0530

    test

--- app.js
+++ app.js
@@ -120,60 +120,9 @@ function processMessage(event) {
 
 function findMovie(userId, movieTitle) {
   console.log("Inside findMovie");
-    request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var movieObj = JSON.parse(body);
-            if (movieObj.Response === "True") {
-                var query = {user_id: userId};
-                var update = {
-                    user_id: userId,
-                    title: movieObj.Title,
-                    plot: movieObj.Plot,
-                    date: movieObj.Released,
-                    runtime: movieObj.Runtime,
-                    director: movieObj.Director,
-                    cast: movieObj.Actors,
-                    rating: movieObj.imdbRating,
-                    poster_url:movieObj.Poster
-                };
-                var options = {upsert: true};
-                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
-                    if (err) {
-                        console.log("Database error: " + err);
-                    } else {
-                        message = {
-                            attachment: {
-                                type: "template",
-                                payload: {
-                                    template_type: "generic",
-                                    elements: [{
-                                        title: movieObj.Title,
-                                        subtitle: "Is this the movie you are looking for?",
-                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
-                                        buttons: [{
-                                            type: "postback",
-                                            title: "Yes",
-                                            payload: "Correct"
-                                        }, {
-                                            type: "postback",
-                                            title: "No",
-                                            payload: "Incorrect"
-                                        }]
-                                    }]
-                                }
-                            }
-                        };
-                        sendMessage(userId, message);
-                    }
-                });
-            } else {
-                console.log(movieObj.Error);
-                sendMessage(userId, {text: movieObj.Error});
-            }
-        } else {
+
             sendMessage(userId, {text: "HI"});
-        }
-    });
+
 }
 
 function getMovieDetail(userId, field) {
@@ -189,6 +138,9 @@ function getMovieDetail(userId, field) {
 
 // sends message to user
 function sendMessage(recipientId, message) {
+  console.log("Inside send message");
+  console.log("recipientId",recipientId);
+  console.log("message",message);
     request({
         url: "https://graph.facebook.com/v2.6/me/messages",
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
@@ -200,6 +152,10 @@ function sendMessage(recipientId, message) {
     }, function(error, response, body) {
         if (error) {
             console.log("Error sending message: " + response.error);
+        }
+        else {
+          console.log("body",body);
+console.log("response",response);
         }
     });
 }
